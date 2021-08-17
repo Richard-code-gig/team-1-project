f01b05d78fe57d030e40ca2d0ee9b6dff94eeb0c
Dhanji97 <m-a.dhanji@hotmail.com>
2021-08-17 16:07:09 +0100

    added unit test for remove_card_data

---
+++ test/test_remove_card_data.py
@@ -0,0 +1,9 @@
+from unittest import mock
+from src.remove_card_data import remove_card_data
+from unittest.mock import Mock
+
+def test_column_removed():
+    mock_df = Mock()
+    mock_df.return_value = {}
+    remove_card_data(mock_df)
+    mock_df.drop.assert_called_with(columns = ['card_no'], inplace=True)
