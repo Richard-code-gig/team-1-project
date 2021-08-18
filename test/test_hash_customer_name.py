99f99f813cbea7ef9ba3d8f382f762c1ca753409
Dhanji97 <m-a.dhanji@hotmail.com>
2021-08-18 12:42:43 +0100

    Unit tests for hash_64 and hash_customer_name

---
+++ test/test_hash_customer_name.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from src.hash_customer_name import hash_customer_name, hash_64
+
+mock_data = {'ID': [1,2,3], 'customer_hash': ['Dave', 'Jane','Dave']}
+
+mock_df = pd.DataFrame(data=mock_data)
+
+def test_hash_64():
+    assert hash_64('Dave') != 'Dave'
+
+def test_hash_customer_name():
+    df = hash_customer_name(mock_df)
+    assert df['customer_hash'].iloc[0] == hash_64('Dave')
+
+def test_consistant_hash():
+    df = hash_customer_name(mock_df)
+    assert df['customer_hash'].iloc[0] == df['customer_hash'].iloc[2]
+
+def test_diffrent_hash():
+    df = hash_customer_name(mock_df)
+    assert df['customer_hash'].iloc[0] != df['customer_hash'].iloc[1]
