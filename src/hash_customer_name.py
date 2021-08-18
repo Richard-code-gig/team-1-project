efa88062d23e4d31027b7cb1628a941ab92b8647
Dhanji97 <m-a.dhanji@hotmail.com>
2021-08-18 12:38:46 +0100

    used new hashing method as builtin hash() is randomised each run

--- src/hash_customer_name.py
+++ src/hash_customer_name.py
@@ -1,8 +1,14 @@
-import pandas as pd
+import base64
+
+def hash_64(name):
+    name_bytes = name.encode('ascii')
+    base64_bytes = base64.b64encode(name_bytes)
+    base64_name = base64_bytes.decode('ascii')
+    return base64_name
 
 def hash_customer_name(df):
     col = 'customer_hash'
     for i in range(len(df[col])):
         customer_name = df[col].iloc[i]
-        df[col].iloc[i] = hash(customer_name)
+        df[col].iloc[i] = hash_64(customer_name)
     return df
