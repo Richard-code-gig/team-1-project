03c57b4a7a7aaeb63cde89a906a0aad7adc1f0fa
Dhanji97 <m-a.dhanji@hotmail.com>
2021-08-25 11:31:16 +0100

    added comments to the code

--- src/data_sensitivity/hash_customer_name.py
+++ src/data_sensitivity/hash_customer_name.py
@@ -1,11 +1,13 @@
 import hashlib
 
 def hash_64(name):
+    '''Hash value "name" using sha256'''
     encoded = name.encode()
     result = hashlib.sha256(encoded)
     return result.hexdigest()
 
 def hash_customer_name(df):
+    '''Replaces values of "customer_hash" in the df with their hashed version'''
     col = 'customer_hash'
     df[col] = df[col].apply(hash_64)
     return df
--- src/data_sensitivity/remove_card_data.py
+++ src/data_sensitivity/remove_card_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def remove_card_data(df):
+    '''Drops "card_no" column from the df'''
     drop = ['card_no']
     df.drop(columns = drop, inplace=True)
     return df
